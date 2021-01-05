dd5ac918fb72101f79d18a31fef25bdea7a9f9bd
Ben Cooper <ben.cooper.226@gmail.com>
2021-01-05 10:40:45 +0000

    Initial draft of data preprocessing routines

--- README.md
+++ README.md
@@ -1,14 +1,20 @@
 # MIMIC_ICU
-Code to define and compare existing ICU readmission risk scores within the MIMIC-III database. This consists of three core scripts.
+Code to define and compare existing ICU readmission risk scores within the MIMIC-III database. This consists of four core scripts.
 
 ## extract_patients
 
 This script uses the MIMIC-III database and filters by eligibility criteria to produce a dataset of `usable' ICU patients, which can be redefined at any time. For data security reasons, this dataset file will not be uploaded to github.
 
+## preprocess_data
+
+For all patients defined in *extract_patients* this script pre-processes all MIMIC data concerning their demographic information and ICU stay. This process largely follows the workflow laid out by Lin et al. 2018 [^1] and available in Python code at https://github.com/Jeffreylin0925/MIMIC-III_ICU_Readmission_Analysis and https://github.com/YerevaNN/mimic3-benchmarks. It outputs all demographic, stay/transfer and input (diagnosis/procedure/prescription) data for selected patients. Events data (chart, lab or output) are not processed, as they are both very large, and not needed in their entirity for subsequent analyses. This will be added in if needed.
+
 ## define_variables
 
-Independant of which patients are included, this script defines and calculates the variables required by each of the three risk scores, and outputs this to another file, also not uploaded.
+Independent of which patients are included, this script defines and calculates the input and outcome variables required by each of the three risk scores, and outputs this to another file, also not uploaded.
 
 ## compare_scores
 
 This script takes the variables calculated above, and uses them to generate the three risk scores. Scores are then compared in the standard method.
+
+[^1]: Lin Y-W, Zhou Y, Faghri F, Shaw MJ, Campbell RH (2019) Analysis and prediction of unplanned intensive care unit readmission using recurrent neural networks with long short-term memory. PLoS ONE 14(7): e0218942. https://doi.org/10.1371/journal.pone.0218942
---
+++ preprocess_data.R
@@ -0,0 +1,148 @@
+# Adapted into R from https://github.com/Jeffreylin0925/MIMIC-III_ICU_Readmission_Analysis
+# and https://github.com/YerevaNN/mimic3-benchmarks
+
+# Load packages
+require(tidyverse)
+require(rms)
+require(brms)
+require(magrittr)
+require(lubridate)
+
+# Load MIMIC database
+source("functions/mimic_load.R")
+
+# Functions
+in_unit_mortality <- function(df) #add_inunit_mortality_to_icustays
+{
+  mortality <- !is.na(df$deathtime) & ((df$intime <= df$deathtime) & (df$outtime >= df$deathtime))
+  mortality = mortality | !is.na(df$deathtime) & 
+                          !is.na(df$dod) & 
+                          ((df$intime <= df$dod) & (df$outtime >= df$dod))
+  return(mortality)
+}
+
+in_hospital_mortality <- function(df) #add_inhospital_mortality_to_icustays
+{
+  mortality <- !is.na(df$deathtime) & ((df$admittime <= df$deathtime) & (df$dischtime >= df$deathtime))
+  mortality <-  mortality | !is.na(df$deathtime) & 
+    !is.na(df$dod) & 
+    ((df$intime <= df$dod) & (df$outtime >= df$dod))
+  return(mortality)
+}
+
+# Load valid patients
+valid_patients <- read_csv("data/valid_patients.csv") %>% 
+  select(subject_id) %>%  deframe()
+
+
+# Read and filter stay data-------------------------------------------------
+
+# Merge and filter transfers (40-41)
+transfers <- mimic$transfers %>% 
+  inner_join(mimic$admissions,
+             by = c("subject_id", "hadm_id")) %>% 
+  inner_join(mimic$patients, by = "subject_id") %>% 
+  filter(subject_id %in% valid_patients) %>% 
+  collect()
+
+# Merge and filter stays (43-44)
+stays <- mimic$icustays %>% 
+  inner_join(mimic$admissions,
+             by = c("subject_id", "hadm_id")) %>% 
+  inner_join(mimic$patients, by = "subject_id")%>% 
+  filter(subject_id %in% valid_patients) %>% 
+  collect()
+
+# Add time events to transfers (46-49)
+transfers %<>% 
+  mutate(age = (intime - dob) / 364.25) %>% 
+  mutate(age = ifelse(age > 90, 90, age)) %>% 
+  mutate(in_unit_mortality = in_unit_mortality(.),
+         in_hospital_mortality = in_hospital_mortality(.)) %>% 
+  filter(age >= 18)
+
+# Add time events to stays (54-57)
+stays %<>% 
+  mutate(age = (intime - dob) / 364.25) %>% 
+  mutate(age = ifelse(age > 90, 90, age)) %>% 
+  mutate(in_unit_mortality = in_unit_mortality(.),
+         in_hospital_mortality = in_hospital_mortality(.)) %>% 
+  filter(age >= 18)
+
+# Should now be saved to csv (62-65)
+
+# Read and filter input data per stay----------------------------------
+
+# Read and translate diagnoses (68)
+diagnoses <- mimic$d_icd_diagnoses %>% 
+  select(2:4) %>% 
+  inner_join(mimic$diagnoses_icd, by = "icd9_code") %>% 
+  collect()
+
+# Filter diagnoses (69)
+diagnoses %<>% 
+  inner_join(stays %>% select(subject_id, hadm_id, icustay_id),
+             by = c("subject_id", "hadm_id"))
+
+# Count diagnosis codes (72)
+diagnosis_counts <- diagnoses %>% 
+  select(icd9_code, hadm_id) %>% 
+  unique() %>% 
+  group_by(icd9_code) %>% 
+  summarise(count = n()) %>% 
+  left_join(mimic$d_icd_diagnoses %>% 
+              select(2:4) %>% 
+              collect())
+
+# Read and translate procedures (76)
+procedures <- mimic$d_icd_procedures %>% 
+  select(2:4) %>% 
+  inner_join(mimic$procedures_icd, by = "icd9_code") %>% 
+  collect()
+
+# Filter procedures (77)
+procedures %<>% 
+  inner_join(stays %>% select(subject_id, hadm_id, icustay_id),
+             by = c("subject_id", "hadm_id"))
+
+# Count procedure codes (80)
+procedure_counts <- procedures %>% 
+  select(icd9_code, hadm_id) %>% 
+  unique() %>% 
+  group_by(icd9_code) %>% 
+  summarise(count = n()) %>% 
+  left_join(mimic$d_icd_procedures %>% 
+              select(2:4) %>% 
+              collect())
+
+# Read and filter prescription data
+prescriptions <- mimic$prescriptions %>% 
+  collect() %>% 
+  filter(subject_id %in% valid_patients)
+
+# Prepare data for writing-----------------------
+
+# Create blank list
+mimic_preproc <- list()
+
+# Insert stay data
+mimic_preproc[[1]] <- valid_patients
+mimic_preproc[[2]] <- transfers
+mimic_preproc[[3]] <- stays
+names(mimic_preproc)[1:3] <- c("patients", "transfers", 
+                               "stays")
+
+# Insert input data
+mimic_preproc[[4]] <- diagnoses
+mimic_preproc[[5]] <- procedures
+mimic_preproc[[6]] <- prescriptions
+names(mimic_preproc)[4:6] <- c("diagnoses", "procedures", 
+                               "prescriptions")
+
+# Insert counts
+mimic_preproc[[7]] <- list("procedure" = procedure_counts,
+                           "diagnosis" = diagnosis_counts)
+names(mimic_preproc)[7] <- "counts"
+
+# Write data
+write_rds(mimic_preproc, "data/mimic_preprocessed.RDS")
