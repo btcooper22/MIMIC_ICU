06a0a4904751e77253a40565144daf9831806e41
Ben Cooper <ben.cooper.226@gmail.com>
2021-01-06 11:24:45 +0000

    Basic framework for APACHE-II calculation function

---
+++ functions/apache_ii.R
@@ -0,0 +1,217 @@
+# Modified from https://github.com/bgulbis/icuriskr/blob/master/R/apache2.R
+require(purrr)
+
+apacheII_score <- function()
+{
+  
+  apache2_score.temp <- function(x, ...) {
+    score <- function(y) {
+      dplyr::case_when(
+        y >= 41 | y <= 29.9 ~ 4L,
+        y >= 39 | y <= 31.9 ~ 3L,
+        y <= 33.9 ~ 2L,
+        y >= 38.5 | y <= 35.9 ~ 1L,
+        is.numeric(y) ~ 0L
+      )
+    }
+    
+    purrr::map_int(x, score)
+  }
+  
+  apache2_score.map <- function(x, ...) {
+    score <- function(y) {
+      dplyr::case_when(
+        y >= 160 | y <= 49 ~ 4L,
+        y >= 130 ~ 3L,
+        y >= 110 | y <= 69 ~ 2L,
+        is.numeric(y) ~ 0L
+      )
+    }
+    
+    purrr::map_int(x, score)
+  }
+  
+  apache2_score.hr <- function(x, ...) {
+    score <- function(y) {
+      dplyr::case_when(
+        y >= 180 | y <= 39 ~ 4L,
+        y >= 140 | y <= 54 ~ 3L,
+        y >= 110 | y <= 69 ~ 2L,
+        is.numeric(y) ~ 0L
+      )
+    }
+    
+    purrr::map_int(x, score)
+  }
+  
+  apache2_score.rr <- function(x, ...) {
+    score <- function(y) {
+      dplyr::case_when(
+        y >= 50 | y <= 5 ~ 4L,
+        y >= 35 ~ 3L,
+        y <= 9 ~ 2L,
+        y >= 25 | y <= 11 ~ 1L,
+        is.numeric(y) ~ 0L
+      )
+    }
+    
+    purrr::map_int(x, score)
+  }
+  
+  apache2_score.ph <- function(x, ...) {
+    score <- function(y) {
+      dplyr::case_when(
+        y >= 7.7 | y < 7.15 ~ 4L,
+        y >= 7.6 | y <= 7.24 ~ 3L,
+        y <= 7.32 ~ 2L,
+        y >= 7.5 ~ 1L,
+        is.numeric(y) ~ 0L
+      )
+    }
+    
+    purrr::map_int(x, score)
+  }
+  
+  apache2_score.sodium <- function(x, ...) {
+    score <- function(y) {
+      dplyr::case_when(
+        y >= 180 | y <= 110 ~ 4L,
+        y >= 160 | y <= 119 ~ 3L,
+        y >= 155 | y <= 129 ~ 2L,
+        y >= 150 ~ 1L,
+        is.numeric(y) ~ 0L
+      )
+    }
+    
+    purrr::map_int(x, score)
+  }
+  
+  apache2_score.potassium <- function(x, ...) {
+    score <- function(y) {
+      dplyr::case_when(
+        y >= 7 | y < 2.5 ~ 4L,
+        y >= 6 ~ 3L,
+        y <= 2.9 ~ 2L,
+        y >= 5.5 | y <= 3.4 ~ 1L,
+        is.numeric(y) ~ 0L
+      )
+    }
+    
+    purrr::map_int(x, score)
+  }
+  
+  apache2_score.scr <- function(x, ...) {
+    score <- function(y) {
+      dplyr::case_when(
+        y >= 3.5 ~ 4L,
+        y >= 2 ~ 3L,
+        y >= 1.5 | y < 0.6 ~ 2L,
+        is.numeric(y) ~ 0L
+      )
+    }
+    
+    purrr::map_int(x, score)
+  }
+  
+  apache2_score.hct <- function(x, ...) {
+    score <- function(y) {
+      dplyr::case_when(
+        y >= 60 | y < 20 ~ 4L,
+        y >= 50 | y <= 29.9 ~ 2L,
+        y >= 46 ~ 1L,
+        is.numeric(y) ~ 0L
+      )
+    }
+    
+    purrr::map_int(x, score)
+  }
+  
+  apache2_score.wbc <- function(x, ...) {
+    score <- function(y) {
+      dplyr::case_when(
+        y >= 40 | y < 1 ~ 4L,
+        y >= 20 | y <= 2.9 ~ 2L,
+        y >= 15 ~ 1L,
+        is.numeric(y) ~ 0L
+      )
+    }
+    
+    purrr::map_int(x, score)
+  }
+  
+  apache2_score.gcs <- function(x, ...) {
+    purrr::map_int(x, ~ 15L - as.integer(.x))
+  }
+  
+  apache2_score.hco3 <- function(x, ...) {
+    score <- function(y) {
+      dplyr::case_when(
+        y >= 52 | y < 15 ~ 4L,
+        y >= 41 | y <= 17.9 ~ 3L,
+        y <= 21.9 ~ 2L,
+        y >= 32 ~ 1L,
+        is.numeric(y) ~ 0L
+      )
+    }
+    
+    purrr::map_int(x, score)
+  }
+  
+  apache2_score.pao2 <- function(x, ...) {
+    score <- function(y) {
+      dplyr::case_when(
+        y < 55 ~ 4L,
+        y <= 60 ~ 3L,
+        y <= 70 ~ 1L,
+        is.numeric(y) ~ 0L
+      )
+    }
+    
+    purrr::map_int(x, score)
+  }
+  
+  apache2_score.aa_grad <- function(x, ...) {
+    score <- function(y) {
+      dplyr::case_when(
+        y >= 500 ~ 4L,
+        y >= 350 ~ 3L,
+        y >= 200 ~ 2L,
+        is.numeric(y) ~ 0L
+      )
+    }
+    
+    purrr::map_int(x, score)
+  }
+  
+  apache2_score.age <- function(x, ...) {
+    score <- function(y) {
+      dplyr::case_when(
+        y >= 75 ~ 6L,
+        y >= 65 ~ 5L,
+        y >= 55 ~ 3L,
+        y >= 45 ~ 2L,
+        is.numeric(y) ~ 0L
+      )
+    }
+    
+    purrr::map_int(x, score)
+  }
+  
+  apache2_score.admit <- function(x, ..., comorbidity) {
+    score <- function(y, z) {
+      if (is.na(z) | z == FALSE) {
+        0L
+      } else {
+        if (is.na(y) | y == "elective") {
+          2L
+        } else {
+          5L
+        }
+      }
+    }
+    
+    purrr::map2_int(x, comorbidity, score)
+  }
+}
+
+
