83754a45d6860d2c72cf27d78e07b4856d653d2d
Ben Cooper <ben.cooper.226@gmail.com>
2021-01-06 12:09:52 +0000

    Function to calculate fluid balance

---
+++ functions/fluid_balance.R
@@ -0,0 +1,10 @@
+# Adapted from concepts in https://github.com/MIT-LCP/mimic-code/tree/master/concepts
+
+# https://github.com/MIT-LCP/mimic-code/blob/master/concepts/fluid_balance/colloid_bolus.sql
+# https://github.com/MIT-LCP/mimic-code/blob/master/concepts/fluid_balance/crystalloid_bolus.sql
+# https://github.com/MIT-LCP/mimic-code/blob/master/concepts/fluid_balance/urine_output.sql
+
+fluid_balance <- function()
+{
+  
+}
