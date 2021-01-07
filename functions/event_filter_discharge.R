a7a1d6b9477f98b0a2799555b7e90ac259f4f7a7
Ben Cooper <ben.cooper.226@gmail.com>
2021-01-07 17:02:51 +0000

    Rewrote to always count in hours

--- functions/event_filter_discharge.R
+++ functions/event_filter_discharge.R
@@ -3,7 +3,8 @@ event_filter_discharge <- function(.df, discharge_time)
   return(
     .df %>% 
       # Calculate time difference from discharge
-      mutate(before_discharge = discharge_time - charttime) %>% 
+      mutate(before_discharge = difftime(discharge_time, charttime,
+                                         units = "hours")) %>% 
       # Select only events before discharge
       filter(before_discharge > 0)
   )
