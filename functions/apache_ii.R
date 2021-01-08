1500135d29ede9630add1858ae836248c9c8fed5
Ben Cooper <ben.cooper.226@gmail.com>
2021-01-08 12:00:23 +0000

    Added temperature and mean arterial pressure

--- functions/apache_ii.R
+++ functions/apache_ii.R
@@ -1,36 +1,78 @@
 # Modified from https://github.com/bgulbis/icuriskr/blob/master/R/apache2.R
+# MIMIC extraction code modified from https://github.com/MIT-LCP/mimic-code/tree/master/concepts/cookbook
 require(purrr)
 
-apacheII_score <- function()
+apacheII_score <- function(labs_df, chart_df, admission_time)
 {
+  # For debug
+  labs_df <- labs
+  chart_df <- charts
+  admission_time <- admit_time
   
-  apache2_score.temp <- function(x, ...) {
-    score <- function(y) {
-      dplyr::case_when(
-        y >= 41 | y <= 29.9 ~ 4L,
-        y >= 39 | y <= 31.9 ~ 3L,
-        y <= 33.9 ~ 2L,
-        y >= 38.5 | y <= 35.9 ~ 1L,
-        is.numeric(y) ~ 0L
-      )
-    }
-    
-    purrr::map_int(x, score)
+  # Helper function: Find closest chart measurement to admission
+  filter_closest <- function(.df, time)
+  {
+    .df %>% 
+      mutate(after_admission = difftime(CHARTTIME, time,
+                                        units = "hours")) %>% 
+      filter(after_admission > 0) %>% 
+      slice_min(after_admission)
   }
   
-  apache2_score.map <- function(x, ...) {
-    score <- function(y) {
-      dplyr::case_when(
-        y >= 160 | y <= 49 ~ 4L,
-        y >= 130 ~ 3L,
-        y >= 110 | y <= 69 ~ 2L,
-        is.numeric(y) ~ 0L
-      )
-    }
-    
-    purrr::map_int(x, score)
-  }
+  # Calculate temperature---------
   
+  # Gather all temperature measurements
+  temp_measurements <- chart_df %>% 
+    filter(ITEMID %in% c(676, 677, 678, 679, 223761, 223762)) %>% 
+    # Convert any F to C
+    mutate(value_degC = ifelse(ITEMID %in% c(678, 679, 223761),
+                               (VALUENUM - 32) * 5/9,
+                               VALUENUM)
+           )
+  
+  # Find closest to ICU admission 
+  temp_value <- temp_measurements %>% 
+    filter_closest(admission_time) %>% 
+    select(value_degC) %>% deframe()
+  
+  # Score
+  temp_score <- case_when(
+    temp_value >= 41 | temp_value <= 29.9 ~ 4L,
+    temp_value >= 39 | temp_value <= 31.9 ~ 3L,
+    temp_value <= 33.9 ~ 2L,
+    temp_value >= 38.5 | temp_value <= 35.9 ~ 1L,
+    is.numeric(temp_value) ~ 0L
+  )
+  
+  # Calculate mean arterial pressure----------
+  
+  # Find first systolic BP measurement
+  systolic_bp <- chart_df %>% 
+    filter(ITEMID %in% c(6, 51, 455, 6701, 220050, 220179)) %>% 
+    # Find closest
+    filter_closest(admission_time) %>% 
+    # Extract sBP
+    select(VALUENUM) %>% deframe()
+  
+  # Find first diastolic BP measurement
+  diastolic_bp <- chart_df %>% 
+    filter(ITEMID %in% c(8364, 8368, 8441, 
+                         8555, 220051, 220180)) %>% 
+    # Find closest
+    filter_closest(admission_time) %>% 
+    # Extract sBP
+    select(VALUENUM) %>% deframe()
+  
+  # Calculate MAP and score
+  map_value <- (systolic_bp + (2 * diastolic_bp) )/ 3
+  map_score <- case_when(
+    map_value >= 160 | map_value <= 49 ~ 4L,
+    map_value >= 130 ~ 3L,
+    map_value >= 110 | map_value <= 69 ~ 2L,
+    is.numeric(map_value) ~ 0L
+  )
+
+  # Heart rate (ventricular response)
   apache2_score.hr <- function(x, ...) {
     score <- function(y) {
       dplyr::case_when(
@@ -44,6 +86,7 @@ apacheII_score <- function()
     purrr::map_int(x, score)
   }
   
+  # Respiratory rate (b/min)
   apache2_score.rr <- function(x, ...) {
     score <- function(y) {
       dplyr::case_when(
@@ -58,6 +101,7 @@ apacheII_score <- function()
     purrr::map_int(x, score)
   }
   
+  # Arterial pH
   apache2_score.ph <- function(x, ...) {
     score <- function(y) {
       dplyr::case_when(
@@ -72,6 +116,7 @@ apacheII_score <- function()
     purrr::map_int(x, score)
   }
   
+  # Serum sodium (mMol/L)
   apache2_score.sodium <- function(x, ...) {
     score <- function(y) {
       dplyr::case_when(
@@ -86,6 +131,7 @@ apacheII_score <- function()
     purrr::map_int(x, score)
   }
   
+  # Serum potassium (mMol/L)
   apache2_score.potassium <- function(x, ...) {
     score <- function(y) {
       dplyr::case_when(
@@ -100,6 +146,7 @@ apacheII_score <- function()
     purrr::map_int(x, score)
   }
   
+  # Serum Creatinine (mg/dL)
   apache2_score.scr <- function(x, ...) {
     score <- function(y) {
       dplyr::case_when(
@@ -113,6 +160,7 @@ apacheII_score <- function()
     purrr::map_int(x, score)
   }
   
+  # Haematocrit (%)
   apache2_score.hct <- function(x, ...) {
     score <- function(y) {
       dplyr::case_when(
@@ -126,6 +174,7 @@ apacheII_score <- function()
     purrr::map_int(x, score)
   }
   
+  # White blood count (1000s)
   apache2_score.wbc <- function(x, ...) {
     score <- function(y) {
       dplyr::case_when(
@@ -139,10 +188,12 @@ apacheII_score <- function()
     purrr::map_int(x, score)
   }
   
+  # Glasgow coma score (units)
   apache2_score.gcs <- function(x, ...) {
     purrr::map_int(x, ~ 15L - as.integer(.x))
   }
   
+  # Serum bicarbonate (venous mMol/L, only use if no aaDo2 or Pa02)
   apache2_score.hco3 <- function(x, ...) {
     score <- function(y) {
       dplyr::case_when(
@@ -157,6 +208,7 @@ apacheII_score <- function()
     purrr::map_int(x, score)
   }
   
+  # Arterial O2 (mmHg)
   apache2_score.pao2 <- function(x, ...) {
     score <- function(y) {
       dplyr::case_when(
@@ -170,7 +222,9 @@ apacheII_score <- function()
     purrr::map_int(x, score)
   }
   
+  # Alveolar–arterial gradient (mmHg)
   apache2_score.aa_grad <- function(x, ...) {
+    
     score <- function(y) {
       dplyr::case_when(
         y >= 500 ~ 4L,
@@ -183,6 +237,27 @@ apacheII_score <- function()
     purrr::map_int(x, score)
   }
   
+  # Calculate AA gradient
+  aa_gradient <- function(pco2, pao2, fio2 = 21, temp = 37, elev = 0) {
+    # pco2: partial pressure of carbon dioxide in arterial blood
+    # pao2: partial pressure of oxygen in arterial blood
+    # fio2:  Fraction of Inspired Oxygen
+    # temp: patient temperature in degrees Celsius
+    # elev  elevation above sea level in meters
+    
+    # Aa DO2 = (FIO2 * (Patm - PH2O) - (PaCO2 / 0.8)) - PaO2
+    # Patm = 760 * exp(Elevation / -7000); Houston elevation = 43 feet (13.106 m)
+    # PH2O = 47 * exp((Temp - 37) / 18.4)
+    
+    patm <- 760 * exp(elev / -7000)
+    ph2o <- 47 * exp((temp - 37) / 18.4)
+    
+    fio2 <- purrr::map_dbl(fio2, ~ dplyr::if_else(.x > 1, .x / 100, .x))
+    
+    (fio2 * (patm - ph2o) - (pco2 / 0.8)) - pao2
+  }
+  
+  # Age
   apache2_score.age <- function(x, ...) {
     score <- function(y) {
       dplyr::case_when(
@@ -197,6 +272,7 @@ apacheII_score <- function()
     purrr::map_int(x, score)
   }
   
+  # Need elective admission + comorbs
   apache2_score.admit <- function(x, ..., comorbidity) {
     score <- function(y, z) {
       if (is.na(z) | z == FALSE) {
