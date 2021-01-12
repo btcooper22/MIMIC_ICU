509ddcdec3a62b0bd8fb13a723dd33026a807e99
Ben Cooper <ben.cooper.226@gmail.com>
2021-01-12 17:22:28 +0000

    Added logistic support for non-linear linkage

--- functions/nomogram_convert.R
+++ functions/nomogram_convert.R
@@ -1,9 +1,10 @@
 nomogram_convert <- function(input_variable,
-                             input_system, output_system)
+                             input_system, output_system,
+                             log = FALSE)
 {
   # Debug
-  # input_system <- age_score_system_input
-  # output_system <- age_score_system_output
+  # input_system <- points_system_input
+  # output_system <- points_system_output
   
   # Create input model
   input_model <- lm(pix ~ input, input_system) %>% 
@@ -12,10 +13,22 @@ nomogram_convert <- function(input_variable,
   # Calculate input pixels
   input_pixels <- input_model[1] + (input_variable * input_model[2])
   
-  # Create output model
-  output_model <- lm(output ~ pix, output_system) %>% 
-    coefficients()
-  
-  # Calculate output variable
-  output_model[1] + (input_pixels * output_model[2])
+
+  if(log == TRUE)
+  {
+    # Create output model
+    output_model <- lm(log(output) ~ pix, output_system) %>% 
+      coefficients()
+    
+    # Calculate output variable
+    return(inverse_logit(output_model[1] + (input_pixels * output_model[2])))
+  }else
+  {
+    # Create output model
+    output_model <- lm(output ~ pix, output_system) %>% 
+      coefficients()
+    
+    # Calculate output variable
+    return(output_model[1] + (input_pixels * output_model[2]))
+  }
 }
