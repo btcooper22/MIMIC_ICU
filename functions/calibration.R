4b7ad0896d4227ca3ff4e68feea35c22bd1ab3e0
Ben Cooper <ben.cooper.226@gmail.com>
2021-01-13 12:06:10 +0000

    Improved calibration function to require less external preprocessing

--- functions/calibration.R
+++ functions/calibration.R
@@ -1,6 +1,8 @@
-calibration <- function(.df, probs_name)
+calibration <- function(.df, probs_name, quantiles_name)
 {
   .df %>% 
+  select(any_of(c("readmission", probs_name, quantiles_name))) %>% 
+    group_by_at(quantiles_name) %>%
     summarise(N = length(readmission),
               observed = (sum(readmission) / length(readmission)) * 100,
               predicted = mean(get(probs_name) * 100),
