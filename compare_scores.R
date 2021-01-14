1e2b49a2184abc2dcf5c2d0d87238e5968c2b127
Ben Cooper <ben.cooper.226@gmail.com>
2021-01-14 10:19:21 +0000

    Clearer cross tabulation output

--- compare_scores.R
+++ compare_scores.R
@@ -100,14 +100,22 @@ patients %<>%
 # Table 1-------------
 
 
-# Crosstabulation function (combine over readmission column)
+# Crosstabulation function
 crosstab <- function(varname, .df = patients)
 {
+  options(dplyr.summarise.inform=F)
+  
   # Find
   .df  %>% 
     group_by_at(c("readmission", varname)) %>% 
     summarise(n = n()) %>% 
-    mutate(`%` = (n/sum(n) * 100))
+    mutate(`%` = (n/sum(n) * 100)) %>% 
+    mutate(`n (%)` = paste(n, " (", round(`%`,1),
+                           "%)", sep = "")) %>% 
+    select(any_of(c("readmission", varname, "n (%)"))) %>% 
+    pivot_wider(names_from = "readmission",
+                values_from = "n (%)") %>% 
+    as.data.frame()
 }
 
 ### Hammer variables
