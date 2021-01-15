486224ecd8a4b017d12fc3aa65750abcbf4c7712
Ben Cooper <ben.cooper.226@gmail.com>
2021-01-15 15:48:02 +0000

    Added calibration and discrimination for recalibrated models

--- compare_scores.R
+++ compare_scores.R
@@ -502,43 +502,63 @@ if(file.exists("data/cooper_model.RDS"))
   write_rds(cooper_model, "data/cooper_model.RDS")
 }
 
+### Compare coefficients
+
 ### Predict on new data
 hammer_rc_probs <- predict(hammer_recalibrate, newdata = patients_validate) %>% inverse_logit()
 martin_rc_probs <- predict(martin_recalibrate, newdata = patients_validate) %>% inverse_logit()
 frost_rc_probs <- predict(frost_recalibrate, newdata = patients_validate) %>% inverse_logit()
 apache_rc_probs <- predict(apache_recalibrate, newdata = patients_validate) %>% inverse_logit()
-cooper_probs <- predict(cooper_model, newdata = patients_validate) %>% inverse_logit()
+cooper_rc_probs <- predict(cooper_model, newdata = patients_validate) %>% inverse_logit()
 
 ### Assess discrimination
 
 # Create prediction objects
 prediction_rc_hammer <- prediction(hammer_rc_probs, patients_validate$readmission)
-prediction_rc_hammer <- prediction(hammer_rc_probs, patients_validate$readmission)
+prediction_rc_martin <- prediction(martin_rc_probs, patients_validate$readmission)
+prediction_rc_frost <- prediction(frost_rc_probs, patients_validate$readmission)
+prediction_rc_apache <- prediction(apache_rc_probs, patients_validate$readmission)
+prediction_rc_cooper <- prediction(cooper_rc_probs, patients_validate$readmission)
 
 # Create performance objects
 performance_rc_hammer <- performance(prediction_rc_hammer, "tpr", "fpr")
+performance_rc_martin <- performance(prediction_rc_martin, "tpr", "fpr")
+performance_rc_frost <- performance(prediction_rc_frost, "tpr", "fpr")
+performance_rc_apache <- performance(prediction_rc_apache, "tpr", "fpr")
+performance_rc_cooper <- performance(prediction_rc_cooper, "tpr", "fpr")
 
 # Create AUC objects
 auc_rc_hammer <- performance(prediction_rc_hammer, measure = "auc")
+auc_rc_martin <- performance(prediction_rc_martin, measure = "auc")
+auc_rc_frost <- performance(prediction_rc_frost, measure = "auc")
+auc_rc_apache <- performance(prediction_rc_apache, measure = "auc")
+auc_rc_cooper <- performance(prediction_rc_cooper, measure = "auc")
 
 # Print AUC
 auc_rc_hammer@y.values[[1]]
+auc_rc_martin@y.values[[1]]
+auc_rc_frost@y.values[[1]]
+auc_rc_apache@y.values[[1]]
+auc_rc_cooper@y.values[[1]]
 
 
 # Plot AUC
-data.frame(x = performance_hammer@x.values[[1]],
-           y = performance_hammer@y.values[[1]],
+data.frame(x = performance_rc_hammer@x.values[[1]],
+           y = performance_rc_hammer@y.values[[1]],
            model = "Hammer") %>% 
   rbind(
-    data.frame(x = performance_martin@x.values[[1]],
-               y = performance_martin@y.values[[1]],
+    data.frame(x = performance_rc_martin@x.values[[1]],
+               y = performance_rc_martin@y.values[[1]],
                model = "Martin"),
-    data.frame(x = performance_frost@x.values[[1]],
-               y = performance_frost@y.values[[1]],
+    data.frame(x = performance_rc_frost@x.values[[1]],
+               y = performance_rc_frost@y.values[[1]],
                model = "Frost"),
-    data.frame(x = performance_apache@x.values[[1]],
-               y = performance_apache@y.values[[1]],
-               model = "APACHE-II")
+    data.frame(x = performance_rc_apache@x.values[[1]],
+               y = performance_rc_apache@y.values[[1]],
+               model = "APACHE-II"),
+    data.frame(x = performance_rc_cooper@x.values[[1]],
+               y = performance_rc_cooper@y.values[[1]],
+               model = "Cooper")
   ) %>% 
   ggplot(aes(x, y, colour = model))+
   geom_abline(slope = 1, intercept = 0,
@@ -552,5 +572,128 @@ data.frame(x = performance_hammer@x.values[[1]],
   scale_color_brewer(palette = "Set1",
                      name = "")
 
+### Assess calibration
+
+# Split data into deciles
+deciles_rc <- tibble(
+  patient_id = 1:nrow(patients_validate),
+  readmission = patients_validate$readmission == "Readmitted to ICU",
+  hammer_rc_probs,
+  decile_hammer = ntile(hammer_rc_probs, 10),
+  martin_rc_probs,
+  decile_martin = ntile(martin_rc_probs, 10),
+  frost_rc_probs,
+  decile_frost = ntile(frost_rc_probs, 10),
+  apache_rc_probs,
+  decile_apache = ntile(apache_rc_probs, 10),
+  cooper_rc_probs,
+  decile_cooper = ntile(cooper_rc_probs, 10),
+)
+
+# Calculate calibration
+cal_hammer <- calibration(deciles_rc, "hammer_rc_probs", "decile_hammer") %>% 
+  mutate(model = "hammer")
+cal_martin <- calibration(deciles_rc, "martin_rc_probs", "decile_martin") %>% 
+  mutate(model = "martin")
+cal_frost <- calibration(deciles_rc, "frost_rc_probs", "decile_frost") %>% 
+  mutate(model = "frost")
+cal_apache <- calibration(deciles_rc, "apache_rc_probs", "decile_apache") %>% 
+  mutate(model = "apache")
+cal_cooper <- calibration(deciles_rc, "cooper_rc_probs", "decile_cooper") %>% 
+  mutate(model = "cooper")
+
+# Plot
+rbind(cal_hammer %>% select(-decile_hammer), 
+      cal_martin %>% select(-decile_martin),
+      cal_frost %>% select(-decile_frost), 
+      cal_apache %>% select(-decile_apache),
+      cal_cooper %>% select(-decile_cooper)) %>% 
+  ggplot(aes(predicted, observed ,
+             colour = model))+
+  geom_abline(slope = 1, intercept = 0,
+              size = 1)+
+  geom_path(size = 1)+
+  geom_pointrange(aes(ymin = observed - error ,
+                      ymax = observed + error,
+                      y = observed,
+                      x = predicted,
+                      colour = model)) +
+  theme_classic(20)+
+  theme(legend.position = "top")+
+  labs(x = "Predicted readmission",
+       y = "Observed readmission")+
+  scale_colour_brewer(palette = "Set1",
+                      name = "")+
+  facet_wrap(~model, scales = "fixed")
 
 
+# Calculate hosmer-lemeshow chi-squared
+hoslem_hammer <- hoslem.test(patients_validate$readmission == "Readmitted to ICU",
+            hammer_rc_probs, g = 10)
+hoslem_martin <- hoslem.test(patients_validate$readmission == "Readmitted to ICU",
+            martin_rc_probs, g = 10)
+hoslem_frost <- hoslem.test(patients_validate$readmission == "Readmitted to ICU",
+            frost_rc_probs, g = 10)
+hoslem_apache <- hoslem.test(patients_validate$readmission == "Readmitted to ICU",
+            apache_rc_probs, g = 10)
+hoslem_cooper <- hoslem.test(patients_validate$readmission == "Readmitted to ICU",
+            cooper_rc_probs, g = 10)
+hoslem_hammer
+hoslem_martin
+hoslem_frost
+hoslem_apache
+hoslem_cooper
+
+# Calculate brier scores
+brier_df <- rbind(
+  brier_extraction(patients_validate$readmission == "Readmitted to ICU", hammer_rc_probs) %>% 
+    mutate(model = "hammer"),
+  brier_extraction(patients_validate$readmission == "Readmitted to ICU", martin_rc_probs) %>% 
+    mutate(model = "martin"),  
+  brier_extraction(patients_validate$readmission == "Readmitted to ICU", frost_rc_probs) %>% 
+    mutate(model = "frost"),
+  brier_extraction(patients_validate$readmission == "Readmitted to ICU", apache_rc_probs) %>% 
+    mutate(model = "apache"),
+  brier_extraction(patients_validate$readmission == "Readmitted to ICU", cooper_rc_probs) %>% 
+    mutate(model = "cooper")
+)
+
+# Plot brier scores
+brier_df %>% 
+  pivot_longer(1:3) %>% 
+  ggplot(aes(x = model, y = value))+
+  geom_bar(stat = "identity", colour = "black",
+           aes(fill = model))+
+  facet_wrap(~name, scales = "free_y")+
+  theme_classic(20)+
+  theme(legend.position = "none")+
+  labs(x = "", y = "")+
+  scale_fill_brewer(palette = "Set1",
+                    name = "")
+
+# Plot combined
+data.frame(
+  model = c("hammer", "martin", "frost", "apache", "cooper"),
+  discrimination = c(auc_rc_hammer@y.values[[1]],
+                     auc_rc_martin@y.values[[1]],
+                     auc_rc_frost@y.values[[1]],
+                     auc_rc_apache@y.values[[1]],
+                     auc_rc_cooper@y.values[[1]]),
+  calibration = c(hoslem_hammer$statistic,
+                  hoslem_martin$statistic,
+                  hoslem_frost$statistic,
+                  hoslem_apache$statistic,
+                  hoslem_cooper$statistic)
+) %>% 
+  ggplot(aes(x = discrimination,
+             y = calibration))+
+  geom_point(aes(fill = model),
+             size = 6,
+             shape = 21)+
+  theme_classic(20)+
+  scale_fill_brewer(palette = "Set1")+
+  scale_y_reverse()+
+  labs(x = "Discrimination (AUROC)",
+       y = expression(paste("Calibration", chi)))
+
+       
