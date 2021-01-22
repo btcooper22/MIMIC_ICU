50ab539be58c8209106526fecd6fba011ca495e5
Ben Cooper <ben.cooper.226@gmail.com>
2021-01-22 12:32:22 +0000

    Experimented unsuccessfully with shrinkage

--- scripts_phase2/1_bayesian_models.R
+++ scripts_phase2/1_bayesian_models.R
@@ -33,7 +33,8 @@ y <- lasso_df$readmission
 cv <- cv.glmnet(x, y, alpha = 1)
 
 # Fit LASSO cooper model
-cooper_model <- glmnet(x, y, alpha = 1, lambda = cv$lambda.min)
+cooper_frequentist <- glmnet(x, y, alpha = 1, lambda = cv$lambda.min,
+                             family = "binomial")
 
 # Fit Bayesian models----------
 hammer_bayes <- brm(readmission ~ sex + general_surgery + cardiac_surgery + hyperglycemia +
@@ -46,24 +47,54 @@ hammer_bayes <- brm(readmission ~ sex + general_surgery + cardiac_surgery + hype
                     cores = 8,
                     file = "models/hammer_bayes")
 
+# Create Cooper formula
 formu <- paste(names(train_df)[feature_id], collapse = " + ")
+
+# Create scaled dataset
+scale_df <- train_df %>% 
+  select(all_of(c(7, feature_id))) %>% 
+  mutate(apache_II = scale(apache_II),
+         serum_glucose = scale(serum_glucose),
+         respiratory_rate = scale(respiratory_rate),
+         final_temp = scale(final_temp),
+         final_SpO2 = scale(final_SpO2),
+         final_lactate = scale(final_lactate),
+         final_bp = scale(final_bp),
+         apache_II_discharge = scale(apache_II_discharge))
 
 cooper_bayes <- brm(formula(paste("readmission ~", formu,
                                   sep = " ")),
                     data = train_df, 
-                    prior = set_prior(lasso(df = cv$lambda.min)),
+                    prior = set_prior(horseshoe(df = 1, par_ratio = 1)),
                     family = bernoulli(link = "logit"),
                     warmup = 500,
                     iter = 6250,
                     chains = 8,
                     cores = 8,
-                    file = "models/cooper_bayes")
+                    control = list(adapt_delta = 0.95))
 
 # Compare: Hammer----------
 
 # Add into one data frame
-fixef(hammer_bayes) %>% 
+fixef(hammer_bayes, robust = TRUE) %>% 
+  as.data.frame() %>%
+  rownames_to_column("Variable") %>% 
+  select(Variable, Estimate, Q2.5, Q97.5) %>% 
   as_tibble() %>% 
-  rownames_to_column() %>% 
-  select(Estimate, Q2.5, Q97.5)
+  rename(Bayesian = "Estimate") %>% 
+  mutate(Frequentist = coefficients(hammer_frequentist)) %>% 
+  select(Variable, Frequentist, Bayesian, Q2.5, Q97.5)
+
+# Compare: Cooper--------
+
+# Extract posteriors
+posterior_df <- posterior_samples(cooper_bayes)
+
+# Rescale
+test <- posterior_df %>% 
+  mutate(b_apache_II = (b_apache_II * attributes(scale_df$apache_II)[[3]]))
+
+# Summarise
+fixef(cooper_bayes)
+cooper_frequentist$beta
 
