0d3ac6a881059bf2794043ee44025d961348c38a
Ben Cooper <ben.cooper.226@gmail.com>
2021-01-29 12:05:27 +0000

    First working draft (missing still missing)

--- Bayesian_Hammer/app.R
+++ Bayesian_Hammer/app.R
@@ -5,6 +5,14 @@ require(readr)
 require(tibble)
 require(shiny)
 require(shinyWidgets)
+require(ggplot2)
+require(tidybayes)
+
+inverse_logit <- function(logit){
+    odds <- exp(logit)
+    prob <- odds / (1 + odds)
+    return(prob)
+}
 
 # Read posteriors
 # post_df <- read_rds("Bayesian_Hammer/hammer_posteriors.RDS")
@@ -15,18 +23,6 @@ predict_posterior <- function(pst_df, sx = FALSE, gsurg = FALSE, csurg = FALSE,
                               hyper = FALSE, apache = FALSE, fluid = FALSE, amb = FALSE,
                               los = FALSE)
 {
-    # Debug
-    # sx <- male
-    # gsurg <- general_surgery
-    # csurg <- cardiac_surgery
-    # hyper <- hyperglycemia
-    # apache <- high_apache
-    # fluid <- fluid_balance_5L
-    # amb <- ambulation
-    # los <- los_5
-    
-    pst_df <- hammer_pst
-    
     # Combine variables
     varlist <- c(sx, gsurg, csurg, hyper, apache, fluid, amb, los)
     
@@ -50,6 +46,7 @@ predict_posterior <- function(pst_df, sx = FALSE, gsurg = FALSE, csurg = FALSE,
 # Define UI for application that draws a histogram
 ui <- fluidPage(
     titlePanel("Bayesian Estimation of Risk Uncertainty from Hammer model"),
+    #---------
     sidebarLayout(sidebarPanel(
                     switchInput(
                         inputId = "male",
@@ -87,8 +84,8 @@ ui <- fluidPage(
                         labelWidth = "120px",
                         offLabel = "No",
                         onLabel = "Yes",
-                        onStatus = "danger", 
-                        offStatus = "success"
+                        onStatus = "success", 
+                        offStatus = "danger"
                     ),
                     materialSwitch(
                         inputId = "missing.cardiac_surgery",
@@ -170,14 +167,105 @@ ui <- fluidPage(
                         label = "", 
                         value = FALSE,
                         status = "warning",
-                    ),
+                    )
              ),
-             mainPanel(plotOutput("distPlot")))
+             #-----------
+             mainPanel(plotOutput("distPlot"),
+                       sliderInput("CI",
+                                   "Highest Density Interval (%):",
+                                   min = 5,
+                                   max = 100,
+                                   value = 95,
+                                   step = 5),
+                       materialSwitch(
+                           inputId = "plotZoom",
+                           label = "Zoom plot",
+                           status = "primary",
+                           right = TRUE
+                       )))
 )
+
+# Debug
+input <- data.frame(
+    male = TRUE,
+    general_surgery = TRUE,
+    cardiac_surgery = FALSE,
+    hyperglycemia = FALSE,
+    high_apache = TRUE,
+    fluid_balance_5L = FALSE,
+    ambulation = TRUE,
+    los_5 = TRUE
+)
+input$CI <- 95
+input$plotZoom <- TRUE
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-
+    output$distPlot <- renderPlot({
+        
+        # Produce prediction posterior
+        probs <- predict_posterior(post_df, input$male, input$general_surgery, input$cardiac_surgery,
+                          input$hyperglycemia, input$high_apache, input$fluid_balance_5L,
+                          !input$ambulation, input$los_5)
+        # Credible interval
+        if(input$CI != 100)
+        {
+            CI_width <- input$CI / 100
+        }else
+        {
+            CI_width <- 0.99999999
+        }
+        risk_CI <-  point_interval(probs * 100, .point = Mode,
+                                   .interval = hdci, .width = CI_width)
+        # Obtain color
+        fill_value <- ifelse(risk_CI$y <= 5, "#4daf4a",
+                             ifelse(risk_CI$y <= 15, "#ffff33",
+                                    ifelse(risk_CI$y <= 50, "#ff7f00", "#e41a1c")))
+        
+        # Draw plot
+        if(input$plotZoom == TRUE)
+        {
+            outplot <- data.frame(probs = probs * 100) %>% 
+                ggplot(aes(x = probs))+
+                geom_density(fill = fill_value, alpha = 0.5)+
+                theme_classic()+
+                labs(x = "Predicted risk of ICU readmission",
+                     y = "Confidence",
+                     title = paste("Risk = ",
+                                   round(risk_CI$y, 2), "% [",
+                                   round(risk_CI$ymin, 2), ", ",
+                                   round(risk_CI$ymax, 2), "]", sep = ""))+
+                theme(axis.text.y = element_blank(),
+                      axis.ticks.y = element_blank())+
+                geom_vline(xintercept = risk_CI$y)+
+                geom_vline(xintercept = c(risk_CI$ymin,
+                                          risk_CI$ymax),
+                           linetype = "dashed")+
+                theme(plot.title = element_text(size=26))
+            
+        }else
+        {
+            outplot <- data.frame(probs = probs * 100) %>% 
+                ggplot(aes(x = probs))+
+                geom_density(fill = fill_value, alpha = 0.5)+
+                theme_classic()+
+                labs(x = "Predicted risk of ICU readmission",
+                     y = "Confidence",
+                     title = paste("Risk = ",
+                                   round(risk_CI$y, 2), "% [",
+                                   round(risk_CI$ymin, 2), ", ",
+                                   round(risk_CI$ymax, 2), "]", sep = ""))+
+                theme(axis.text.y = element_blank(),
+                      axis.ticks.y = element_blank())+
+                geom_vline(xintercept = risk_CI$y)+
+                geom_vline(xintercept = c(risk_CI$ymin,
+                                          risk_CI$ymax),
+                           linetype = "dashed")+
+                coord_cartesian(xlim=c(0, 100)) +
+                theme(plot.title = element_text(size=26))
+        }
+        outplot
+    })
 }
 
 # Run the application 
