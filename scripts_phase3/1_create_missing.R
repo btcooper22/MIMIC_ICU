474fd1dbb802fc089bbc03a74183a5d3fd6e3d8b
Ben Cooper <ben.cooper.226@gmail.com>
2021-02-04 12:29:07 +0000

    Moved calibration and discrimination to compare_imputations

--- scripts_phase3/1_create_missing.R
+++ scripts_phase3/1_create_missing.R
@@ -37,56 +37,6 @@ summary(full_data)
 # Write
 write_csv(full_data,"data/impute/complete_cases.csv")
 
-# Create and assess APACHE model------
-# Create model and predict
-apache_model <- glm(readmission ~ apache_II_discharge,
-                    data = full_data, family = "binomial")
-write_rds(apache_model, "models/apache_model.RDS")
-probs <- predict(apache_model, newdata = full_data) %>% inverse_logit()
-
-# Assess ROC
-pred <- prediction(probs, full_data$readmission)
-perf <- performance(pred, "tpr", "fpr")
-auc <- performance(pred, measure = "auc")
-auc@y.values[[1]]
-
-# Plot ROC
-data.frame(x = perf@x.values[[1]],
-           y = perf@y.values[[1]]) %>% 
-  ggplot(aes(x, y))+
-  geom_abline(slope = 1, intercept = 0,
-              linetype = "dotted",
-              size = 1)+
-  geom_path(size = 1)+
-  labs(x = "1 - Specificity",
-       y = "Sensitivity")+
-  theme_classic(20)
-
-# Split into deciles
-deciles_df <- tibble(
-  patient_id = 1:nrow(full_data),
-  readmission = full_data$readmission,
-  probs,
-  decile = ntile(probs, 10))
-
-# Calculate calibration
-cal <- calibration(deciles_df, "probs", "decile")
-
-# Plot calibration
-cal %>% 
-  ggplot(aes(predicted, observed))+
-  geom_abline(slope = 1, intercept = 0,
-              size = 1)+
-  geom_path(size = 1)+
-  geom_pointrange(aes(ymin = observed - error ,
-                      ymax = observed + error,
-                      y = observed,
-                      x = predicted)) +
-  theme_classic(20)+
-  theme(legend.position = "top")+
-  labs(x = "Predicted readmission",
-       y = "Observed readmission")
-
 # Simulate MCAR (missing completely at random)---------
 # Set n
 N <- 100
