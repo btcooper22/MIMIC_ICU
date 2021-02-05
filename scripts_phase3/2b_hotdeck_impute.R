bdda926c42026b60d50c6efd049bb97292fcdcbf
Ben Cooper <ben.cooper.226@gmail.com>
2021-02-05 12:18:15 +0000

    First full working draft

---
+++ scripts_phase3/2b_hotdeck_impute.R
@@ -0,0 +1,202 @@
+# Packages
+require(dplyr)
+require(magrittr)
+require(readr)
+require(tibble)
+require(foreach)
+require(stringr)
+require(doParallel)
+require(tools)
+require(tidyr)
+require(forcats)
+
+# Functions
+source("functions/filename_metadata.R")
+source("functions/make_pool.R")
+source("functions/parallel_setup.R")
+
+# Load data
+full_data <- read_csv("data/impute/complete_cases.csv") 
+
+# Load MIMIC database
+source("functions/mimic_load.R")
+
+# Find admissions data
+admissions <- mimic$admissions %>% 
+  select(hadm_id, diagnosis,
+         admission_type) %>% 
+  collect() %>% 
+  rename(adm_id = "hadm_id")
+
+# Join to database
+full_data %<>% 
+  left_join(admissions)
+
+# Create combinations list
+pooling_vars <- list(
+  c("admission_type", "apache_chronic_discharge",
+    "apache_age_discharge", "diagnosis"),
+  c("apache_chronic_discharge",
+    "apache_age_discharge", "diagnosis"),
+  c("admission_type",
+    "apache_age_discharge", "diagnosis"),
+  c("admission_type", "apache_chronic_discharge",
+    "diagnosis"),
+  c("admission_type", "apache_chronic_discharge",
+    "apache_age_discharge"),
+  c("admission_type", "apache_age_discharge"),
+  c("admission_type", "apache_chronic_discharge"),
+  c("admission_type", "diagnosis"),
+  c("apache_age_discharge", "apache_chronic_discharge"),
+  c("apache_age_discharge", "diagnosis"),
+  c("apache_chronic_discharge", "diagnosis"),
+  "admission_type",
+  "apache_age_discharge",
+  "apache_chronic_discharge",
+  "diagnosis"
+)
+
+# Main loop through files-------
+
+# Load model for prediction
+apache_model <- read_rds("models/apache_model.RDS")
+
+# Locate files
+files <- dir("data/MCAR/", full.names = TRUE)
+
+# Set up parallel
+ptm <- proc.time()
+parallel_setup(8)
+
+output <- foreach(f = 1:length(files), .packages = c("dplyr", "tidyr",
+                                                     "stringr", "tibble",
+                                                     "magrittr", "forcats")) %dopar%
+  {
+    # Load file
+    mcar_df <- read.csv(files[f]) %>% 
+    # Join pooling data
+    cbind(full_data %>% 
+            select(admission_type,
+                   diagnosis))
+    
+    # Find NA columns
+    mcar_NA <- mcar_df %>% 
+      select_if(function(x) any(is.na(x))) %>% 
+      # Add identifying data again
+      cbind(mcar_df[13:16])
+    
+    # Loop through NA columns
+    imputed_NA <- foreach(i = 1:(ncol(mcar_NA)-4)) %do%
+      {
+        # Find column name
+        col_name <- names(mcar_NA)[i]
+        
+        # Identify data
+        NA_variable <- subset(mcar_NA, is.na(mcar_NA[,i]))
+        
+        # Identify complete cases
+        present_variable <- subset(mcar_NA, !is.na(mcar_NA[,i]))
+      
+        # Loop through pooling options
+        replace_values <- foreach(j = 1:length(pooling_vars)) %do%
+        {
+          # Debug
+          print(paste(i,j))
+          
+          # Generate unique pooling groups
+          if(length(pooling_vars[[j]]) > 1)
+          {
+            NA_variable$pooling_group_var <- apply(NA_variable[,pooling_vars[[j]]],
+                                                   1, paste, collapse = "_" )
+          }else
+          {
+            NA_variable$pooling_group_var <- NA_variable[,pooling_vars[[j]]]
+          }
+          
+          # Loop through pool groups
+          foreach(k = 1:length(unique(NA_variable$pooling_group_var)),
+                  .combine = "c") %do%
+            {
+              # Extract identifier
+              identifier <- unique(NA_variable$pooling_group_var)[k]
+              
+              # Generate candidate pool
+              candidate_pool <- make_pool(pooling_vars[[j]],
+                                          NA_variable %>% 
+                                            filter(pooling_group_var == identifier),
+                                          present_variable)
+              
+              # Sample from candidate pool
+              sample(candidate_pool[,col_name],
+                     sum(NA_variable$pooling_group_var == identifier),
+                     replace = TRUE)
+            }
+        }
+        
+        # Replace NAs for each method
+        imputed_col <- foreach(j = 1:length(replace_values)) %do%
+          {
+            fullcol <- mcar_NA[,i]
+            fullcol[is.na(fullcol)] <- replace_values[[j]]
+            fullcol
+          }
+        
+        # Output
+        imputed_col
+      }
+    
+    # Reattach
+    full_imputed <- foreach(i = 1:length(pooling_vars),
+                            .combine = "cbind") %do%
+      {
+        full_df <- foreach(j = 1:(ncol(mcar_df)-4),
+                .combine = "cbind") %do%
+          {
+            # i is methods, j is columns
+            if(any(is.na(mcar_df[,j])))
+            {
+              # Replace with imputed
+              col_values <- imputed_NA[[j]][[i]]
+            }else
+            {
+              # Do not replace
+              col_values <- mcar_df[,j]
+            }
+            
+            # Output
+            col_values
+          }
+        
+        # Attach remaining values
+        full_df %<>% as.data.frame() %>% 
+          cbind(mcar_df[,13:14])
+        names(full_df) <- names(mcar_df)[1:14]
+        
+        # Sum and predict
+        probs <-  full_df %>% 
+          mutate(apache_II_discharge = rowSums(full_df)) %>% 
+          predict(apache_model, newdata = .)
+        
+        # Output
+        probs
+      }
+    
+    # Extract metadata
+    metadata <- filename_metadata(files[f])
+    
+    # Create output
+    output <- list(pooling = pooling_vars,
+         probs = full_imputed)
+    
+    # Create filename
+    filename <- paste("data/impute/hotdeck/hotdeck_S",
+                      str_pad(metadata[2], 4, "right", 0),"_N",
+                      str_pad(metadata[1],3,"left",0), ".RDS", sep = "")
+    
+    # Write to file
+    write_rds(output, filename,
+              compress = "gz")
+    
+  }
+stopImplicitCluster()
+proc.time() - ptm()
