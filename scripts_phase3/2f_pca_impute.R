f17748079aeb814f68d484458d8f3f6c79a03fca
Ben Cooper <ben.cooper.226@gmail.com>
2021-02-10 11:26:56 +0000

    Added predicting and filename writing

--- scripts_phase3/2f_pca_impute.R
+++ scripts_phase3/2f_pca_impute.R
@@ -30,37 +30,67 @@ ncomps <- 2:11
 ptm <- proc.time()
 parallel_setup(15)
 
-output <- foreach(f = 1:length(files),
-                  .packages = c("foreach", "dplyr",
-                               "missMDA", "stringr")) %dopar%
+all_PCA <- foreach(f = 1:length(files),
+                   .packages = c("foreach", "dplyr",
+                                 "missMDA", "stringr",
+                                 "readr")) %dopar%
   {
-    # Load file
-    mcar_df <- read.csv(files[f])
-    
-    # Loop through ncomps
-    imputed_dfs <- foreach(j = 1:length(ncomps)) %do%
-      {
-        # Select apache variables
-        PCA_results <- mcar_df %>% 
-          select(1:12) %>% 
-          # Perform PCA
-          imputePCA(ncp = ncomps[j])
-        
-        # Return imputed data
-        list(ncomps = j,
-          PCA_results$completeObs)
-      }
-    
     # Extract metadata
     metadata <- filename_metadata(files[f])
-
+    
+    # Create filename
+    filename <- paste("data/impute/PCA/PCA_S",
+                      str_pad(metadata[2], 4, "right", 0),"_N",
+                      str_pad(metadata[1],3,"left",0), ".RDS", sep = "")
+    
+    if(!file.exists(filename))
+    {
+      # Load file
+      mcar_df <- read.csv(files[f])
+      
+      # Loop through ncomps
+      imputed_dfs <- foreach(j = 1:length(ncomps)) %do%
+        {
+          print(j)
+          # Select apache variables
+          PCA_results <- mcar_df %>% 
+            select(1:12) %>% 
+            # Perform PCA
+            imputePCA(ncp = ncomps[j])
+          
+          # Return imputed data
+          list(ncomps = j,
+               PCA_results$completeObs)
+        }
+      
+      # Perform predictions and disentangle
+      probs <- foreach(k = 1:length(imputed_dfs),
+                       .combine = "cbind") %do%
+        {
+          imputed_dfs[[k]][[2]] %>% 
+            as.data.frame() %>% 
+            cbind(mcar_df[,13:15]) %>% 
+            mutate(apache_II_discharge = calculate_apache_scores(.)) %>% 
+            predict(apache_model, newdata = .)
+        }
+      
+      # Combine and write
+      output <- list(n = metadata[1],
+                     split = metadata[2],
+                     ncomps,
+                     probs)
+      
+      write_rds(output, filename)
+    } else
+    {
+      output <- read_rds(filename)
+    }
+    
     # Return
-    list(n = metadata[1],
-         split = metadata[2],
-         data = imputed_dfs)
+    output
   }
 stopImplicitCluster()
-proc.time() - ptm # 
+proc.time() - ptm # 115 min
 
-write_rds(output, "data/impute/PCA.RDS",
+write_rds(all_PCA, "data/impute/PCA.RDS",
           compress = "gz")
