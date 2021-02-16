cf32ceaff0334069412606e229cdc0aabbba07d5
Ben Cooper <ben.cooper.226@gmail.com>
2021-02-16 10:42:11 +0000

    Added option to alter parallel process priority

--- functions/parallel_setup.R
+++ functions/parallel_setup.R
@@ -1,9 +1,17 @@
 require(doParallel)
 require(tools)
 
-parallel_setup <- function(n_cores)
+parallel_setup <- function(n_cores, priority = "idle")
 {
-  psnice(value = 19)
+  # Set priority
+  if(priority == "idle")
+  {
+    psnice(value = 19)
+  } else
+  {
+    psnice(value = 15)
+  }
+
   registerDoParallel(ifelse(detectCores() <= n_cores,
                             detectCores() - 1,
                             n_cores)
