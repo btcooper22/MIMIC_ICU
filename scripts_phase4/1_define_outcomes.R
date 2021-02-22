aa52dfcd19612f488813502b7f6b66606c9fc4da
Ben Cooper <ben.cooper.226@gmail.com>
2021-02-22 11:23:46 +0000

    Extract 30 day and in-hospital mortality, filtering patients who died in ICU

--- scripts_phase4/1_define_outcomes.R
+++ scripts_phase4/1_define_outcomes.R
@@ -9,7 +9,7 @@ options(dplyr.summarise.inform = FALSE)
 # Load preprocessed data
 mimic_preproc <- read_rds("data/mimic_preprocessed.RDS")
 
-# Create outcome measures: Readmission within same hospitalisation-------------------
+# Create outcome measures: Mortality-------------------
 
 # Prepare parallel options
 ptm <- proc.time()
@@ -43,52 +43,59 @@ outcomes <- foreach(i = 1:length(mimic_preproc$patients),
   surgical_stay %<>% 
     mutate(readmission = readmission_flag)
   
+  # Check for death
+  if(!is.na(surgical_stay$dod[1]))
+  {
+    # Measure time between ICU discharge and death
+    death_diff <- difftime(surgical_stay$dod[1], 
+             surgical_stay$outtime[nrow(surgical_stay)],
+             unit = "days")
+    
+    # Flag if <30 days
+    surgical_stay$mortality_30d <- death_diff < 30
+  }else
+  {
+    surgical_stay$mortality_30d <- FALSE
+  }
   
-  # # Count stays (126-128)
-  # stays %<>%
-  #   group_by(hadm_id) %>%
-  #   summarise(counts = n()) %>%
-  #   left_join(stays, by  = "hadm_id") %>%
-  #   arrange(row_id.x)
-  #
-  # # Generate output points for each stay (130-132)
-  # stays %<>%
-  #   group_by(hadm_id) %>%
-  #   mutate(max_outtime = max(outtime) == outtime)
-
-  # Detect back transfers to ICU (134)
-  # stays %<>%
-  #   mutate(readmission = (counts > 1) & (max_outtime == 0)) %>%
-  #   # Restrict to surgical hospitalisations
-  #   filter(surgical_stay == TRUE)
-  #
   # # Write all information
   surgical_stay %>%
-    select(subject_id, hadm_id, in_hospital_mortality, readmission) %>%
+    select(subject_id, hadm_id, in_hospital_mortality,
+           mortality_30d, readmission, in_unit_mortality) %>%
     mutate(subject_n = i) %>%
     group_by(hadm_id) %>%
     slice_head()
 }
 stopImplicitCluster()
-proc.time() - ptm # 70 seconds @ 12 cores
+proc.time() - ptm # 60 seconds @ 12 cores
 
 # Clean and write results-------------
 
 # REMOVE ORGAN DONORS
 #WHERE lower(diagnosis) NOT LIKE '%organ donor%'
 
+# All patients who died in hospital flagged as within 30 days?
+any(outcomes$in_hospital_mortality & !outcomes$mortality_30d)
+
+# All patients who died in ICU flagged as in hospital?
+any(outcomes$in_unit_mortality & !outcomes$in_hospital_mortality)
+
+# How many patients died in ICU?
+table(outcomes$in_unit_mortality)
+
+# Filter out in-unit mortality
+outcomes %<>% 
+  filter(in_unit_mortality == FALSE)
+
 # How many patients died in hospital?
 table(outcomes$in_hospital_mortality)
 
-# Remove those who died
-outcomes %<>% filter(in_hospital_mortality == FALSE)
-
-# How many readmissions?
-table(outcomes$readmission)
+# How many died within 30 days
+table(outcomes$mortality_30d)
 
 # Write
 outcomes %>% 
   ungroup() %>% 
   select(subject_id, hadm_id,
-         readmission) %>% 
-  write_csv("data/outcomes.csv")
+         in_hospital_mortality, mortality_30d) %>% 
+  write_csv("data/outcomes_mortality.csv")
