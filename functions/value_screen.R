a9fcb76b9fe8a812c867ab970f93704574aa917c
Ben Cooper <ben.cooper.226@gmail.com>
2021-02-23 09:11:43 +0000

    Option to return inner or outer

--- functions/value_screen.R
+++ functions/value_screen.R
@@ -1,13 +1,25 @@
 value_screen <- function(.df, variable_name,
-                         safe_range)
+                         safe_range, return_safe = FALSE)
 {
-  .df %>% 
+  # Select variable of interest
+  pivoted_df <- .df %>% 
     select(any_of(c(paste(c("a", "d"), variable_name,
                           sep = "_"),
                     "row_id"))) %>% 
     na.omit() %>% 
-    pivot_longer(1:2) %>% 
-    filter(value < safe_range[1] |
-             value > safe_range[2]) %>% 
-    return()
+    pivot_longer(1:2)
+  
+  # Perform filtering
+    if(return_safe)
+    {
+      filt_df <- pivoted_df %>% 
+      filter(value > safe_range[1] &
+               value < safe_range[2])
+    }else
+    {
+      filt_df <- pivoted_df %>% 
+      filter(value < safe_range[1] |
+               value > safe_range[2])
+    }
+    return(filt_df)
 }
