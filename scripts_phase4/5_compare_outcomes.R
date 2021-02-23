52b8a0876d089f118e1882eb279209ce3f0e63a3
Ben Cooper <ben.cooper.226@gmail.com>
2021-02-23 16:27:41 +0000

    Assess discrimination

--- scripts_phase4/5_compare_outcomes.R
+++ scripts_phase4/5_compare_outcomes.R
@@ -123,10 +123,73 @@ data.frame(probs = predict(model_admission_mortInUnit,
            outcome = model_admission_mortInUnit$data$mort_inunit) %>% 
   mutate(probs = inverse_logit(probs)) -> probs_admission_mortInUnit
 
-# Generate AUC values----
+# Assess discrimination----
 
 # Generate prediction objects
 prediction_admission_readmission <- prediction(probs_admission_readmission$probs,
                                                probs_admission_readmission$outcome)
 
-# Generate AUC objects
+prediction_discharge_readmission <- prediction(probs_discharge_readmission$probs,
+                                               probs_discharge_readmission$outcome)
+
+prediction_admission_mort30 <- prediction(probs_admission_mort30$probs,
+                                               probs_admission_mort30$outcome)
+
+prediction_discharge_mort30 <- prediction(probs_discharge_mort30$probs,
+                                               probs_discharge_mort30$outcome)
+
+prediction_admission_mortInHospital <- prediction(probs_admission_mortInHospital$probs,
+                                               probs_admission_mortInHospital$outcome)
+
+prediction_discharge_mortInHospital <- prediction(probs_discharge_mortInHospital$probs,
+                                               probs_discharge_mortInHospital$outcome)
+
+prediction_admission_mortInUnit <- prediction(probs_admission_mortInUnit$probs,
+                                               probs_admission_mortInUnit$outcome)
+
+
+# Generate AUC
+performance(prediction_admission_readmission, measure = "auc")@y.values[[1]]
+performance(prediction_discharge_readmission, measure = "auc")@y.values[[1]]
+performance(prediction_admission_mort30, measure = "auc")@y.values[[1]]
+performance(prediction_discharge_mort30, measure = "auc")@y.values[[1]]
+performance(prediction_admission_mortInHospital, measure = "auc")@y.values[[1]]
+performance(prediction_discharge_mortInHospital, measure = "auc")@y.values[[1]]
+performance(prediction_admission_mortInUnit, measure = "auc")@y.values[[1]]
+
+# Generate performance objects
+performance_admission_mort30 <- performance(prediction_admission_mort30, "tpr", "fpr")
+performance_discharge_mort30 <- performance(prediction_discharge_mort30, "tpr", "fpr")
+performance_admission_mortInHospital <- performance(prediction_admission_mortInHospital, "tpr", "fpr")
+performance_discharge_mortInHospital <- performance(prediction_discharge_mortInHospital, "tpr", "fpr")
+performance_admission_mortInUnit <- performance(prediction_admission_mortInUnit, "tpr", "fpr")
+
+# Plot AUC
+data.frame(x = performance_admission_mort30@x.values[[1]],
+           y = performance_admission_mort30@y.values[[1]],
+           model = "admission_30") %>% 
+  rbind(
+    data.frame(x = performance_discharge_mort30@x.values[[1]],
+               y = performance_discharge_mort30@y.values[[1]],
+               model = "discharge_30"),
+    data.frame(x = performance_admission_mortInHospital@x.values[[1]],
+               y = performance_admission_mortInHospital@y.values[[1]],
+               model = "admission_inhospital"),
+    data.frame(x = performance_discharge_mortInHospital@x.values[[1]],
+               y = performance_discharge_mortInHospital@y.values[[1]],
+               model = "discharge_inhospital"),
+    data.frame(x = performance_admission_mortInUnit@x.values[[1]],
+               y = performance_fialho@y.values[[1]],
+               model = "admission_inunit")
+  ) %>% 
+  ggplot(aes(x, y, colour = model))+
+  geom_abline(slope = 1, intercept = 0,
+              linetype = "dotted",
+              size = 1)+
+  geom_path(size = 1)+
+  labs(x = "1 - Specificity",
+       y = "Sensitivity")+
+  theme_classic(20)+
+  theme(legend.position = "top")+
+  scale_color_brewer(palette = "Set1",
+                     name = "")
