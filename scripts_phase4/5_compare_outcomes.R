0a3edb1ee623830aeb4d94dbbe9a9819f8301b28
Ben Cooper <ben.cooper.226@gmail.com>
2021-02-23 15:36:04 +0000

    Generated predictions

--- scripts_phase4/5_compare_outcomes.R
+++ scripts_phase4/5_compare_outcomes.R
@@ -2,6 +2,9 @@
 require(readr)
 require(dplyr)
 require(tidyr)
+require(ROCR)
+
+source("functions/inverse_logit.R")
 
 # Load data
 results <- read_csv("data/apache_data_full.csv") %>% 
@@ -56,3 +59,58 @@ model_discharge_mortInHospital <- glm(mort_inhosp ~ apache_II,
 model_admission_mortInUnit <- glm(mort_inunit ~ apache_II,
                                       data = admission_results,
                                       family = "binomial")
+
+# Generate predictions----
+
+# admission_readmission
+data.frame(probs = predict(model_admission_readmission, 
+                           newdata = admission_results),
+           outcome = admission_results$readmission) %>% 
+  mutate(probs = inverse_logit(probs)) -> probs_admission_readmission
+
+# discharge_readmission
+data.frame(probs = predict(model_discharge_readmission, 
+                           newdata = discharge_results),
+           outcome = discharge_results$readmission) %>% 
+  mutate(probs = inverse_logit(probs)) -> probs_discharge_readmission
+
+# admission_30d
+data.frame(probs = predict(model_admission_mort30, 
+                           newdata = admission_results),
+           outcome = admission_results$mort_30) %>% 
+  mutate(probs = inverse_logit(probs)) -> probs_admission_mort30
+
+# discharge_30d
+data.frame(probs = predict(model_discharge_mort30, 
+                           newdata = discharge_results),
+           outcome = discharge_results$mort_30) %>% 
+  mutate(probs = inverse_logit(probs)) -> probs_discharge_mort30
+
+# admission_inhosp
+data.frame(probs = predict(model_admission_mortInHospital, 
+                           newdata = admission_results),
+           outcome = admission_results$mort_inhosp) %>% 
+  mutate(probs = inverse_logit(probs)) -> probs_admission_mortInHospital
+
+# discharge_inhosp
+data.frame(probs = predict(model_discharge_mortInHospital, 
+                           newdata = discharge_results),
+           outcome = discharge_results$mort_inhosp) %>% 
+  mutate(probs = inverse_logit(probs)) -> probs_discharge_mortInHospital
+
+# admission_inunit
+data.frame(probs = predict(model_admission_mortInUnit, 
+                           newdata = admission_results),
+           outcome = admission_results$mort_inunit) %>% 
+  mutate(probs = inverse_logit(probs)) -> probs_admission_mortInUnit
+
+# Generate AUC values----
+
+# Generate prediction objects
+prediction_admission_readmission <- prediction(probs_admission_readmission$probs,
+                                               probs_admission_readmission$outcome)
+
+
+# Generate AUC objects
+
+# Generate
