726763d8f61eff6296366e2d2a7b1e8b52d0549d
Ben Cooper <ben.cooper.226@gmail.com>
2021-02-24 17:11:26 +0000

    Modified mice imputation for mortality data

--- scripts_phase4/6c_mice_impute.R
+++ scripts_phase4/6c_mice_impute.R
@@ -6,105 +6,71 @@ require(foreach)
 require(mice)
 require(stringr)
 require(randomForest)
+require(tidyr)
 
 # Functions
-source("functions/calculate_apache_score.R")
-source("functions/filename_metadata.R")
-source("functions/parallel_setup.R")
-
-# Load data-------
-full_data <- read_csv("data/impute/complete_cases.csv") 
-
-# Load model for prediction
-apache_model <- read_rds("models/apache_model.RDS")
 
 # Main loop through files-------
 
-# Locate files
-files <- dir("data/MCAR/", full.names = TRUE)
+# Load data
+source("functions/data_loader_splitter.R")
 
 # Set methods
-mice_methods <- c("pmm","norm.boot",
-                  "norm.predict",
-                  "midastouch")
+mice_methods <- c("pmm", "norm.boot", "norm.predict", "midastouch",
+                  "sample", "norm", "norm.nob",
+                  "quadratic", "ri")
 
-# Set up parallel
-ptm <- proc.time()
-parallel_setup(14)
 
-all_mice <- foreach(f = 1:1200,
-                   .packages = c("foreach", "dplyr",
-                                 "mice", "stringr",
-                                 "readr")) %dopar%
+# Loop through methods
+imputed_dfs <- foreach(j = 1:length(mice_methods)) %do%
   {
-    # Extract metadata
-    metadata <- filename_metadata(files[f])
+    print(j)
     
-    # Create filename
-    filename <- paste("data/impute/MICE/MICE_S",
-                      str_pad(metadata[2], 4, "right", 0),"_N",
-                      str_pad(metadata[1],3,"left",0), ".RDS", sep = "")
+    # Perform imputation
+    MICE_results <- apache_scores %>% 
+      mice(method = mice_methods[j])
     
-    if(!file.exists(filename))
-    {
-      # Load file
-      mcar_df <- read.csv(files[f])
-      
-      # Loop through methods
-      imputed_dfs <- foreach(j = 1:length(mice_methods)) %do%
-        {
-          print(j)
-          
-          # Select apache variables
-          MICE_results <- mcar_df %>% 
-            select(1:12) %>% 
-            # Perform imputation
-            mice(method = mice_methods[j])
-
-          # Return imputed data
-          list(method = mice_methods[j],
-               df1 = complete(MICE_results, 1),
-               df2 = complete(MICE_results, 2),
-               df3 = complete(MICE_results, 3),
-               df4 = complete(MICE_results, 4),
-               df5 = complete(MICE_results, 5))
-        }
-      
-      # Perform predictions and disentangle
-      probs <- foreach(k = 1:length(imputed_dfs),
-                       .combine = "cbind") %do%
-        {
-          # Loop through datasets within a method
-          prob_matrix <- foreach(d = 1:5,
-                  .combine = "cbind") %do%
-          {
-            imputed_dfs[[k]][[d+1]] %>% 
-              as.data.frame() %>% 
-              cbind(mcar_df[,13:15]) %>% 
-              mutate(apache_II_discharge = calculate_apache_scores(.)) %>% 
-              predict(apache_model, newdata = .)
-          }
-          # Return mean
-          rowMeans(prob_matrix)
-        }
-      
-      # Combine and write
-      output <- list(n = metadata[1],
-                     split = metadata[2],
-                     mice_methods,
-                     probs)
-      
-      write_rds(output, filename)
-    } else
-    {
-      output <- read_rds(filename)
-    }
-    
-    # Return
-    output
+    # Return imputed data
+    list(method = mice_methods[j],
+         df1 = complete(MICE_results, 1),
+         df2 = complete(MICE_results, 2),
+         df3 = complete(MICE_results, 3),
+         df4 = complete(MICE_results, 4),
+         df5 = complete(MICE_results, 5))
   }
-stopImplicitCluster()
-proc.time() - ptm # 2 days!!!
 
-write_rds(all_mice, "data/impute/MICE.RDS",
-          compress = "gz")
+# Disentangle
+disentangled_df <- foreach(k = 1:length(imputed_dfs)) %do%
+  {
+    # Loop through datasets within a method
+    value_matrix <- foreach(d = 1:5,
+                           .combine = "cbind") %do%
+      {
+        pivoted_df <- imputed_dfs[[k]][[d+1]] %>% 
+          as.data.frame() %>% 
+          mutate(id = 1:nrow(imputed_dfs[[k]][[d+1]])) %>% 
+          pivot_longer(1:17)
+        
+        if(d == 1)
+        {
+          pivoted_df
+        }else
+        {
+          pivoted_df[,3]
+        }
+      }
+    
+    # Calculate mean
+    means_df <- data.frame(id = value_matrix[,1],
+                           var = value_matrix[,2],
+               value = rowMeans(value_matrix[,3:7]))
+    
+    # Re-pivot
+    means_df %>% 
+      pivot_wider(names_from = var,
+                  values_from = value) %>% 
+      select(-id)
+  }
+names(disentangled_df) <- paste("MICE", mice_methods, sep = "_")
+
+write_rds(disentangled_df, "data/impute_mortality/MICE.RDS")
