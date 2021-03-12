dc4f90a8ca26837e9b54f45f2045ebfaf4d01e76
btcooper22 <ben.cooper.226@gmail.com>
2021-03-12 10:30:09 +0000

    Skeleton of desired variables

---
+++ scripts_phase5/3_define_predictors.R
@@ -0,0 +1,41 @@
+# APACHE-II (and >20)
+
+# Apache vars
+
+# ARF
+
+# > 0 days on resp. support
+
+# > 0  days on advanced CV support
+
+# Basic CV support for  % of stay
+
+# Total support as % of stay
+
+# Any organ support required
+
+# Age
+
+# BMI
+
+# LOS > 2 
+
+# Time in hospital before ICU
+
+# Cardiac surgery
+
+# Medical history vars
+
+# Diabetes
+
+# Prehospital dependency
+
+# Sedation
+
+# Clinically ready for discharge days == 0
+
+# Out-of-hours discharge
+
+# Some expected dependency
+
+# Follow up care days >5
