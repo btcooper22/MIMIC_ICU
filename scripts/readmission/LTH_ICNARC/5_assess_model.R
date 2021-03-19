7c3059b2c593be728c6595200c4547d62b2265a6
btcooper22 <ben.cooper.226@gmail.com>
2021-03-19 12:46:40 +0000

    Extraction of discrimination and validation of bespoke model

--- scripts/readmission/LTH_ICNARC/5_assess_model.R
+++ scripts/readmission/LTH_ICNARC/5_assess_model.R
@@ -71,3 +71,87 @@ output %>%
             cal_error_high = quantile(cal_chisq, 0.75))
 # AUC 0.629 pm 0.053
 # Cal 9.59 [6.81, 13.88]
+
+# AUC  AUC_error      cal cal_error_low cal_error_high
+# 1 0.6182097 0.05428703 9.877884      6.820818       14.20504
+
+# Extract median discrimination and calibration data----
+
+# Discrimination
+set.seed(which.min(abs(output$disc - median(output$disc))))
+
+# Split data
+patients_train <- results %>% 
+  group_by(readmission) %>% 
+  slice_sample(prop = 0.75)
+
+patients_validate <- results %>% 
+  filter(id %in% patients_train$id == FALSE)
+
+# Rebuild model
+final_model <- glm(readmission ~ 
+                     anaemia + apache_II + 
+                     glasgow_coma_below_15 +
+                     high_risk_speciality + length_of_stay +
+                     out_of_hours_discharge + pulse_rate,
+                   data = patients_train,
+                   family = "binomial")
+
+# Create predictions
+probs <- predict(final_model, newdata = patients_validate) %>% inverse_logit()
+
+# Assess discrimination
+pred <- prediction(probs[!is.na(probs)],
+                   patients_validate$readmission[!is.na(probs)])
+
+# Calibration
+set.seed(which.min(abs(output$cal_chisq - median(output$cal_chisq))))
+
+# Split data
+patients_train <- results %>% 
+  group_by(readmission) %>% 
+  slice_sample(prop = 0.75)
+
+patients_validate <- results %>% 
+  filter(id %in% patients_train$id == FALSE)
+
+# Rebuild model
+final_model <- glm(readmission ~ 
+                     anaemia + apache_II + 
+                     glasgow_coma_below_15 +
+                     high_risk_speciality + length_of_stay +
+                     out_of_hours_discharge + pulse_rate,
+                   data = patients_train,
+                   family = "binomial")
+
+# Create predictions
+probs <- predict(final_model, newdata = patients_validate) %>% inverse_logit()
+
+# Split to deciles
+decile_df <- tibble(
+  patient_id = 1:nrow(patients_validate),
+  readmission = patients_validate$readmission,
+  probs,
+  decile = ntile(probs, 10)) %>% 
+  na.omit()
+
+# Measure calibration
+calibration_df <- decile_df %>% 
+  select(readmission, probs, decile) %>% 
+  group_by(decile) %>%
+  summarise(N = length(readmission),
+            observed = (sum(readmission) / length(readmission)) * 100,
+            predicted = mean(probs * 100),
+            error = sd(probs * 100))
+
+# Quantify calibration
+cal_test <- hoslem.test(patients_validate$readmission[!is.na(probs)],
+            probs[!is.na(probs)], g = 10)
+
+# Combine and write
+list(model = "bespoke",
+     data = "LTH_ICNARC",
+     discrimination = pred,
+     calibration = cal_test,
+     deciles = calibration_df) %>% 
+  write_rds("scripts/readmission/shared/data/LTH_ICNARC_bespoke")
